6921e0425b6f7a1a30a3d97ffc1a5ca8c3a175ea
YumNumm <onory0917@gmail.com>
2023-07-03 19:12:25 +0900

    fix

--- src/sample/sample.ts
+++ src/sample/sample.ts
@@ -12,12 +12,14 @@ export namespace EqmonitorTelegramSchemaSample {
   // stream
   export async function sample(socket: Socket) {
     // ディレクトリのファイル一覧を取得 sample_
-    const files = await fs.promises.readdir('./sample/')
+    const files = await fs.promises.readdir('./sample/20171213b/json')
     // すべて読み込む
     const promises = files
       .filter(file => file.endsWith('.json'))
       .map(async file => {
-        const data = await fs.promises.readFile(`./sample/${file}`)
+        const data = await fs.promises.readFile(
+          `./sample/20171213b/json/${file}`,
+        )
         return data.toString()
       })
     const results = await Promise.all(promises)
