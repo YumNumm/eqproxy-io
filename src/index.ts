e10de67985f973eb1b7391838bbb9da79db5b1a2
YumNumm <onory0917@gmail.com>
2023-07-03 19:10:51 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -59,11 +59,11 @@ export const dmdata = new DMDATA({
 })
 
 async function main() {
-  const files = await fs.promises.readdir('./sample')
+  const files = await fs.promises.readdir('./sample/20171213b/json')
   const promises = files
     .filter(file => file.endsWith('.json'))
     .map(async file => {
-      const data = await fs.promises.readFile(`./sample/${file}`)
+      const data = await fs.promises.readFile(`./sample/20171213b/json/${file}`)
       return data.toString()
     })
   const results = await Promise.all(promises)
