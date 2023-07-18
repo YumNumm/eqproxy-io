a1ea2816e3ebff6d29a657c0dc4482dd773c0cec
YumNumm <onory0917@gmail.com>
2023-07-18 21:28:54 +0900

    fix: sample

--- src/sample/sample.ts
+++ src/sample/sample.ts
@@ -3,6 +3,7 @@ import { EqmonitorTelegramSchema } from '../model/telegram_v3'
 import { TelegramJSONMain } from '@dmdata/telegram-json-types'
 import fs from 'fs'
 import { Socket } from 'socket.io'
+import { EewInformation as ei } from '../model/eew-telegram'
 
 export namespace EqmonitorTelegramSchemaSample {
   export function vxse45Sample(): EqmonitorTelegramSchema.TelegramV3[] {
@@ -34,17 +35,28 @@ export namespace EqmonitorTelegramSchemaSample {
         return EqmonitorTelegramSchema.fromDmdataToTelegramV3(data)
       })
       .flat()
+      .map(data => data as ei.EewInformation)
     const start = new Date(converted[0].pressTime)
+    const originTime = new Date(
+      new Date().getTime() -
+        new Date(converted[0].pressTime).getTime() +
+        new Date(
+          converted[0].body.originTime ?? converted[0].body.arrivalTime,
+        ).getTime(),
+    )
     const futures = converted.map(data => {
       // 次のファイルの開始時間を取得
       const next = new Date(data.pressTime)
       // 開始時間からの経過時間を取得
       const diff = next.getTime() - start.getTime()
+
+      // 現在時刻と発生時刻の差分を取得
       // 待機する
       return new Promise(resolve => {
         setTimeout(() => {
           // 送信
           data.status = '訓練'
+          data.body.originTime = originTime.toISOString()
           socket.emit('data', data)
           resolve(data)
         }, diff)
