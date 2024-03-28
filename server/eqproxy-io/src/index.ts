1fb685bf28868b0851cdff912ec0cfde02d6043a
YumNumm <onory0917@gmail.com>
2024-03-29 04:39:51 +0900

    fix

--- server/eqproxy-io/src/index.ts
+++ server/eqproxy-io/src/index.ts
@@ -5,7 +5,6 @@ import { IncomingWebhook } from '@slack/webhook'
 import { config } from './config/config'
 import { websocket } from './websocket/websocket'
 import { startDmDataWs } from './dmdata/dmdata'
-require('newrelic')
 
 export const isProd = process.env.NODE_ENV == 'production'
 export const Logger = getLogger()
--- service/dmdata-proxy/src/index.ts
+++ service/dmdata-proxy/src/index.ts
@@ -1,43 +1,42 @@
-import { DMDATA } from '@dmdata/sdk-js'
-import { getLogger } from 'log4js'
-import { exit } from 'process'
-import { IncomingWebhook } from '@slack/webhook'
-import { config } from './config/config'
-import { startDmDataWs } from './dmdata/dmdata'
-import { startHttpServer, wss } from './websocket/websocket'
-require('newrelic')
+import { DMDATA } from "@dmdata/sdk-js"
+import { getLogger } from "log4js"
+import { exit } from "process"
+import { IncomingWebhook } from "@slack/webhook"
+import { config } from "./config/config"
+import { startDmDataWs } from "./dmdata/dmdata"
+import { startHttpServer, wss } from "./websocket/websocket"
 
 export const Logger = getLogger()
-Logger.level = 'debug'
-export const isProd = process.env.NODE_ENV == 'production'
+Logger.level = "debug"
+export const isProd = process.env.NODE_ENV == "production"
 export const slackWebhook = new IncomingWebhook(config.SLACK_WEBHOOK_URL)
-process.on('uncaughtException', async err => {
+process.on("uncaughtException", async (err) => {
   Logger.error(err)
   await slackWebhook.send({
     username: process.env.SERVERNAME,
     icon_url:
-      'https://avatars.slack-edge.com/2023-05-07/5217296640694_7b1502a5d1890db65d37_48.png',
+      "https://avatars.slack-edge.com/2023-05-07/5217296640694_7b1502a5d1890db65d37_48.png",
     attachments: [
       {
-        pretext: '<!channel>',
-        color: '#ff0000',
+        pretext: "<!channel>",
+        color: "#ff0000",
 
         fields: [
           {
-            title: 'uncaughtException',
-            value: 'uncaughtExceptionが発生しました。終了しました。',
+            title: "uncaughtException",
+            value: "uncaughtExceptionが発生しました。終了しました。",
           },
           {
-            title: 'Error',
+            title: "Error",
             value: err!.toString(),
           },
           {
-            title: 'Stack',
+            title: "Stack",
             // 2行まで
-            value: err!.stack!.split('\n').slice(0, 2).join('\n') + '\n...',
+            value: err!.stack!.split("\n").slice(0, 2).join("\n") + "\n...",
           },
           {
-            title: 'Time',
+            title: "Time",
             value: new Date().toISOString(),
           },
         ],
@@ -45,7 +44,7 @@ process.on('uncaughtException', async err => {
     ],
   })
   // wait 500ms
-  await new Promise(resolve => setTimeout(resolve, 500))
+  await new Promise((resolve) => setTimeout(resolve, 500))
   exit(1)
 })
 
@@ -53,7 +52,7 @@ export const dmdata = new DMDATA({
   credentials: {
     getAuthorization: async () => {
       return (
-        'Basic ' + Buffer.from(config.DMDATA_API_KEY + ':').toString('base64')
+        "Basic " + Buffer.from(config.DMDATA_API_KEY + ":").toString("base64")
       )
     },
   },
@@ -63,7 +62,7 @@ async function main() {
   try {
     startHttpServer()
     startDmDataWs()
-    wss;
+    wss
   } catch (e: any) {
     Logger.error(e)
     exit(1)
