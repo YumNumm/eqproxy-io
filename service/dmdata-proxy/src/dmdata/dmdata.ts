4aae22300dc2e38339d8478b1fc62defc17fa77e
YumNumm <onory0917@gmail.com>
2024-03-29 10:35:21 +0900

    fix: dmdata

--- service/dmdata-proxy/src/dmdata/dmdata.ts
+++ service/dmdata-proxy/src/dmdata/dmdata.ts
@@ -1,107 +1,105 @@
-import { WebSocketService } from '@dmdata/sdk-js';
-import { Logger, dmdata, slackWebhook } from '..';
-import { exit } from 'process';
-import { config } from '../config/config';
-import { wss } from '../websocket/websocket';
+import { WebSocketService } from "@dmdata/sdk-js"
+import { Logger, dmdata, slackWebhook } from ".."
+import { exit } from "process"
+import { config } from "../config/config"
+import { wss } from "../websocket/websocket"
 
-export let webSocketService: WebSocketService;
+export let webSocketService: WebSocketService
 
 export async function startDmDataWs() {
-	Logger.info('Starting WebSocketService');
-	try {
-		webSocketService = await dmdata.socket.start({
-			classifications: ['eew.forecast', 'telegram.earthquake'],
-			appName: config.SERVERNAME,
-			formatMode: 'json',
-			test: 'no',
-		});
-	} catch (e: any) {
-		// axios error
-		if (e.response) {
-			Logger.error(e.response.data);
-		}
-		Logger.error(e);
-		throw e;
-	}
-	Logger.info('WebSocketService started');
+  Logger.info("Starting WebSocketService")
+  try {
+    webSocketService = await dmdata.socket.start({
+      classifications: ["eew.forecast", "telegram.earthquake"],
+      appName: config.SERVERNAME,
+      formatMode: "json",
+      test: "no",
+    })
+  } catch (e: any) {
+    // axios error
+    if (e.response) {
+      Logger.error(e.response.data)
+    }
+    Logger.error(e)
+    throw e
+  }
+  Logger.info("WebSocketService started")
 
-	// data時の処理
-	webSocketService.on('start', async connInfo => {
-		Logger.info(connInfo);
-		await slackWebhook.send({
-			username: process.env.SERVERNAME,
-			icon_url:
-				'https://avatars.slack-edge.com/2023-05-07/5217296640694_7b1502a5d1890db65d37_48.png',
-			attachments: [
-				{
-					color: '#0000FF',
-					fields: [
-						{
-							title: 'DM-D.S.S WebSocketService',
-							value: 'WebSocketが接続されました。',
-						},
-						{
-							title: 'socketId',
-							value: connInfo.socketId!.toString(),
-						},
-					],
-				},
-			],
-			text: 'DM-D.S.S WebSocketService started',
-		});
-	});
-	webSocketService.on('close', async () => {
-		await slackWebhook.send({
-			username: process.env.SERVERNAME,
-			icon_url:
-				'https://avatars.slack-edge.com/2023-05-07/5217296640694_7b1502a5d1890db65d37_48.png',
-			attachments: [
-				{
-					color: '#ff0000',
-					pretext: '<!channel>',
-					fields: [
-						{
-							title: 'DM-D.S.S WebSocketService',
-							value: 'WebSocketが切断されました。終了します。',
-						},
-					],
-				},
-			],
-			text: 'DM-D.S.S WebSocketService closed',
-		});
-		exit(1);
-	});
-	webSocketService.on('error', async err => {
-		await slackWebhook.send({
-			username: process.env.SERVERNAME,
-			icon_url:
-				'https://avatars.slack-edge.com/2023-05-07/5217296640694_7b1502a5d1890db65d37_48.png',
-			attachments: [
-				{
-					color: '#ff0000',
-					fields: [
-						{
-							title: 'DM-D.S.S WebSocketService',
-							value: 'WebSocket接続中にエラーが発生しました。終了します。',
-						},
-						{
-							title: 'Error',
-							value: err.toString(),
-						},
-					],
-				},
-			],
-		});
-		exit(1);
-	});
-	webSocketService.on('data', async data => {
-		Logger.info('Received data from WS');
-		Logger.info(data);
+  // data時の処理
+  webSocketService.on("start", async (connInfo) => {
+    Logger.info(connInfo)
+    await slackWebhook.send({
+      username: process.env.SERVERNAME,
+      icon_url:
+        "https://avatars.slack-edge.com/2023-05-07/5217296640694_7b1502a5d1890db65d37_48.png",
+      attachments: [
+        {
+          color: "#0000FF",
+          fields: [
+            {
+              title: "DM-D.S.S WebSocketService",
+              value: "WebSocketが接続されました。",
+            },
+            {
+              title: "socketId",
+              value: connInfo.socketId!.toString(),
+            },
+          ],
+        },
+      ],
+      text: "DM-D.S.S WebSocketService started",
+    })
+  })
+  webSocketService.on("close", async () => {
+    await slackWebhook.send({
+      username: process.env.SERVERNAME,
+      icon_url:
+        "https://avatars.slack-edge.com/2023-05-07/5217296640694_7b1502a5d1890db65d37_48.png",
+      attachments: [
+        {
+          color: "#ff0000",
+          pretext: "<!channel>",
+          fields: [
+            {
+              title: "DM-D.S.S WebSocketService",
+              value: "WebSocketが切断されました。終了します。",
+            },
+          ],
+        },
+      ],
+      text: "DM-D.S.S WebSocketService closed",
+    })
+    exit(1)
+  })
+  webSocketService.on("error", async (err) => {
+    await slackWebhook.send({
+      username: process.env.SERVERNAME,
+      icon_url:
+        "https://avatars.slack-edge.com/2023-05-07/5217296640694_7b1502a5d1890db65d37_48.png",
+      attachments: [
+        {
+          color: "#ff0000",
+          fields: [
+            {
+              title: "DM-D.S.S WebSocketService",
+              value: "WebSocket接続中にエラーが発生しました。終了します。",
+            },
+            {
+              title: "Error",
+              value: err.toString(),
+            },
+          ],
+        },
+      ],
+    })
+    exit(1)
+  })
+  webSocketService.on("data", async (data) => {
+    Logger.info("Received data from WS")
+    Logger.info(data)
 
-		wss.clients.forEach(client => {
-			if (client.readyState === WebSocket.OPEN) {
-				client.send(JSON.stringify(data));
-			}
-		});
-	});
+    wss.clients.forEach((client) => {
+      client.send(JSON.stringify(data))
+    })
+  })
 }
